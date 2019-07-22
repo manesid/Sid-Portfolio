281b2cc1b969124501b5979154a625795b7cef9f
Sid Mane <manesid3@gmail.com>
2019-07-22 14:34:42 +0530

    Portfolio

---
+++ data/data.js
@@ -0,0 +1,58 @@
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Siddheshwar Mane",
+  description: "Java Developer & Software Engineer.",
+  bio: "Java Developer with two years of IT experience in all phases of SDLC such as Planning,Analysis,Design,Implementation,Testing and Maintenance.",
+  email: "manesid3@gmail.com",
+  location: "Pune, Maharashtra",
+  birth: "1995-05-11", // YYYY-MM-DD format
+  skills: [
+    "JavaFX",
+    "Java",
+    "Spring Boot",
+    "Javalin",
+    "Android",
+    "Javascript",
+    "SQL",
+    "H2"
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/guim-gonzalez-846606134/",
+    twitter: "https://twitter.com/_sidmane",
+    github: "https://github.com/manesid/",
+    stackoverflow: "https://stackoverflow.com/users/4747260/sid-mane",
+    dev: ""
+  },
+  experience: [{
+    title: "Java Developer",
+    company: "TechMainstay Software Pvt. Ltd",
+    url: "http://techmainstay.com/",
+    location: "Nigdi, Pune",
+    description: "Led a team of four developers , SDLC Management , Documentation , Mentored new interns and Client interactions.",
+    from: "August 2017",
+    to: "Now"
+  }],
+  education: [{
+    school: "University Of Pune",
+    degree: "Engineering",
+    fieldOfStudy: "Computer Science",
+    location: "Pune, Maharashatra",
+    from: "July 2017",
+    to: "July 2013"
+  }],
+  quote: "I turn coffee into code"
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "sid"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+  //  - sid
+};
